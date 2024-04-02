47dbba93d9f2acb5870fba50b4311c03d785b90d
meryem <meryemaydmr41@gmail.com>
2024-04-02 16:16:02 +0300

    some fixes

--- hospital-app/src/App.js
+++ hospital-app/src/App.js
@@ -1,48 +1,23 @@
-import "./App.css";
-import Cardd from "./components/card/Card.jsx";
-import Header from "./components/header/Header";
-import { appointmentData, doctorData } from "./components/helper/data.js";
-import AppointmentCard from "./components/appointments/AppointmentCard.jsx";
-import { useState } from "react";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  const [appointmentDatam, setAppointmentDatam]= useState(appointmentData)
-
   return (
     <div className="App">
-      <Header />
-      <div className="container py-4 d-flex align-items-center flex-wrap gap-4">
-        {doctorData.map((doctor) => {
-          return (
-            <Cardd
-              key={doctor.id}
-              doctorName={doctor.name}
-              doctorDep={doctor.dep}
-              doctorImg={doctor.img}
-              setApp={setAppointmentDatam}
-              appoint={appointmentData}
-            />
-          );
-        })}
-      </div>
-
-      <div className="container py-4 d-flex flex-column gap-4">
-        <h2>APPOINTMENT LIST</h2>
-
-        {appointmentDatam.map((item) => (
-          <AppointmentCard
-            key={item.id}
-            doctorName={item.doctor}
-            patientName={item.patient}
-            day={item.day}
-            consulted={item.consulted}
-            setApp={setAppointmentDatam}
-            appoint={appointmentData}
-          />
-        ))}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
