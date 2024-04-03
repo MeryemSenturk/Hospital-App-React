257e40abe3c1b6458c3476f3342c016c88bee2e0
meryem <meryemaydmr41@gmail.com>
2024-04-03 14:15:49 +0300

    dr

---
+++ hospital-app/src/components/Doctors.jsx
@@ -0,0 +1,35 @@
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import { doctorData } from "../helpers/data";
+
+const Doctors = () => {
+  console.log(doctorData);
+  return (
+    <Container>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
+
+      <Row>
+        {doctorData.map((doctor) => {
+          const { id, img, name, dep } = doctor;
+          return (
+            <Col xs={6} sm={4} lg={3} key={id}>
+              <Image
+                className="img-thumbnai doctor-img w-100"
+                src={img}
+                alt={name}
+              />
+              <h5>{name}</h5>
+              <p>{dep}</p>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
+
+export default Doctors;
